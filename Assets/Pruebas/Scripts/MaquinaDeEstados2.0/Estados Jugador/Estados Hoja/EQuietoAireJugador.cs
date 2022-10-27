f0fa04daeaedd39bc51781225ac7c8ac280f7743
FranNogui <116107405+FranNogui@users.noreply.github.com>
2022-10-27 15:29:30 +0200

    Estados preparados y sin errores

--- Assets/Pruebas/Scripts/MaquinaDeEstados2.0/Estados Jugador/Estados Hoja/EQuietoAireJugador.cs
+++ Assets/Pruebas/Scripts/MaquinaDeEstados2.0/Estados Jugador/Estados Hoja/EQuietoAireJugador.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EQuietoAireJugador : MonoBehaviour
+public class EQuietoAireJugador : EstadoJugador
 {
-    // Start is called before the first frame update
-    void Start()
+    public EQuietoAireJugador(MaquinaDeEstadosJugador contextoActual,
+        FabricaDeEstadosJugador fabrica) : base(contextoActual, fabrica) { }
+
+    public override void ComprobarCambioEstado()
     {
-        
+        throw new System.NotImplementedException();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void EntrarEstado()
     {
-        
+        throw new System.NotImplementedException();
+    }
+
+    public override void IniciarSubestado()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SalirEstado()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UpdateEstado()
+    {
+        throw new System.NotImplementedException();
     }
 }
