b7072df385b4c7c43af299bfbb333a555d6a53e6
diego-manuel1 <99796304+diego-manuel1@users.noreply.github.com>
2022-11-25 19:59:22 +0100

    Primera versión de la generación de enemigos

---
+++ Assets/Pruebas/Scripts/MecanicasEnemigo/EnemyGenerator.cs
@@ -0,0 +1,177 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject enemigo1;
+    [SerializeField]
+    private GameObject enemigo2;
+    [SerializeField]
+    private int minEnemigos;
+    [SerializeField]
+    private int maxEnemigos;
+    [SerializeField]
+    private int tiempoEsperaMin;
+    [SerializeField]
+    private int tiempoEsperaMax;
+    [SerializeField]
+    private float alturaGeneracion;
+    private float posY;
+    private float posX;
+    private float posZ;
+    private float longX;
+    private float longY;
+    private float longZ;
+    private float tiempo;
+    private bool activo;
+
+    void Start()
+    {
+        posY = transform.position.y;
+        posX = transform.position.x;
+        posZ = transform.position.z;
+        longX = transform.localScale.x;
+        longY = transform.localScale.y;
+        longZ = transform.localScale.z;
+        tiempo = 0f;
+        activo = false;
+        StartCoroutine(spawn());
+    }
+
+    IEnumerator spawn()
+    {
+        while (true)
+        {
+            int t = UnityEngine.Random.Range(tiempoEsperaMin, tiempoEsperaMax);
+            yield return new WaitForSeconds(t);
+            GenerarEnemigo();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void GenerarEnemigo()
+    {
+        /*if (generadorHorizontal)
+        {
+            float puntoGeneracionX = generadorNumero.Next((int)(posX - longX / 2), (int)(posX + longX / 2));
+            GenerarEnemigoHorizontal(puntoGeneracionX);
+        }
+        else
+        {
+            float puntoGeneracionY = generadorNumero.Next((int)(posY - longY / 2), (int)(posY + longY / 2));
+            GenerarEnemigoVertical(puntoGeneracionY);
+        }*/
+        int numEnemigos = UnityEngine.Random.Range(minEnemigos, maxEnemigos);
+        
+        float[] posEnemigosX = new float[numEnemigos];
+        float[] posEnemigosZ = new float[numEnemigos];
+        for(int i = 0; i < numEnemigos; i++)
+        {
+            float puntoGeneracionX = UnityEngine.Random.Range((posX - longX / 2), (posX + longX / 2));
+            float puntoGeneracionZ = UnityEngine.Random.Range((posZ - longZ / 2), (posZ + longZ / 2));
+            posEnemigosX[i] = puntoGeneracionX;
+            posEnemigosZ[i] = puntoGeneracionZ; 
+        }
+        for(int i = 0; i < numEnemigos; i++)
+        {
+            Instantiate(enemigo1, new Vector3(posEnemigosX[i], alturaGeneracion, posEnemigosZ[i]), transform.rotation);
+        }
+    }
+
+   /* private void GenerarEnemigoHorizontal(float puntoGeneracionX)
+    {
+        float rand = UnityEngine.Random.value;
+        rand *= 100;
+        if (rand < 100 / 4)
+            Instantiate(enemigo1, new Vector2(puntoGeneracionX, posY), transform.rotation);
+        else if (rand < 2 * 100 / 4)
+            Instantiate(enemigo2, new Vector2(puntoGeneracionX, posY), transform.rotation);
+        else if (rand < 3 * 100 / 4)
+            Instantiate(enemigo3, new Vector2(puntoGeneracionX, posY), transform.rotation);
+        else
+            Instantiate(enemigo4, new Vector2(puntoGeneracionX, posY), transform.rotation);
+    }
+
+    private void GenerarEnemigoCentral()
+    {
+        GameObject creado;
+        float rand = UnityEngine.Random.value;
+        rand *= 100;
+        if (rand < 100 / 4)
+            creado = Instantiate(enemigo1, transform.position, transform.rotation);
+        else if (rand < 2 * 100 / 4)
+            creado = Instantiate(enemigo2, transform.position, transform.rotation);
+        else if (rand < 3 * 100 / 4)
+            creado = Instantiate(enemigo3, transform.position, transform.rotation);
+        else
+            creado = Instantiate(enemigo4, transform.position, transform.rotation);
+    }
+
+    private void GenerarEnemigoVertical(float puntoGeneracionY)
+    {
+        float rand = UnityEngine.Random.value;
+        rand *= 100;
+        if (rand < 100 / 4)
+            Instantiate(enemigo1, new Vector2(posX, puntoGeneracionY), transform.rotation);
+        else if (rand < 2 * 100 / 4)
+            Instantiate(enemigo2, new Vector2(posX, puntoGeneracionY), transform.rotation);
+        else if (rand < 3 * 100 / 4)
+            Instantiate(enemigo3, new Vector2(posX, puntoGeneracionY), transform.rotation);
+        else
+            Instantiate(enemigo4, new Vector2(posX, puntoGeneracionY), transform.rotation);
+    }
+
+    public void activar()
+    {
+        activo = true;
+    }
+
+    public void desactivar()
+    {
+        activo = false;
+    }
+
+    public void nivel(int nivel)
+    {
+        switch (nivel)
+        {
+            case 0:
+                enemigo1 = circulo;
+                enemigo2 = circulo;
+                enemigo3 = circulo;
+                enemigo4 = circulo;
+                tiempoEsperaMin = 5;
+                tiempoEsperaMax = 10;
+                break;
+            case 1:
+                enemigo1 = circulo;
+                enemigo2 = circulo;
+                enemigo3 = circulo;
+                enemigo4 = triangulo;
+                tiempoEsperaMin = 5;
+                tiempoEsperaMax = 10;
+                break;
+            case 2:
+                enemigo1 = circulo;
+                enemigo2 = circulo;
+                enemigo3 = circulo;
+                enemigo4 = triangulo;
+                tiempoEsperaMin = 4;
+                tiempoEsperaMax = 6;
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+    }*/
+}
