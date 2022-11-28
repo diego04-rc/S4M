b522bdd495a88ddf7453e81566a779ac939458bf
diego04-rc <diego04-rc@users.noreply.github.com>
2022-11-28 17:29:31 +0100

    Moneda, Vida y Energia v1 sin animaciones

---
+++ Assets/El_Camino_De_S4M/Scripts/Interaccion/Moneda.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moneda : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, 90 * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Jugador") {
+            other.GetComponent<RecogerMonedas>().monedaRecogida();
+            Destroy(gameObject);
+        }
+    }
+}
